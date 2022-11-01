4de79e57e0b743e2011d50315e51d43118ca1248
Yashwa122 <yyousaf2021@elam.co.uk>
2022-11-01 15:35:59 +0000

    AI Stuff

---
+++ W_Undercover/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,73 @@
+/*
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    public NavMeshAgent agent;
+
+    public Transform player;
+
+    public LayerMask whatIsGround, whatIsPlayer;
+
+    //Patrolling
+    public Vector3 walkpoint;
+    bool walkpointset;
+    public float walkpointrange;
+
+    private void Awake()
+    {
+        player = GameObject.Find("FPSPlayer").transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+
+        if (!playerInSightRange && !playerInAttackRange) Patrolling();
+        if (playerInSightRange && !playerInAttackRange) ChasePlayer();
+        if (playerInSightRange && playerInAttackRange) AttackPlayer();
+    }
+
+    private void Patrolling()
+    {
+        if (!walkpointset) SearchWalkPoint();
+
+        if (walkpointset)
+            agent.SetDestination(walkpoint);
+
+        Vector3 distanceToWalkPoint = transform.position - walkpoint;
+
+        //WalkPoint reached
+        if (distanceToWalkPoint.magnitude < 1f)
+            walkpointset = false;
+    }
+
+    private void SearchWalkPoint()
+    {
+        float randomZ = Random.Range(-walkpointrange, walkpointrange);
+        float randomX = Random.Range(-walkpointrange, walkpointrange);
+
+        walkpoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if (Physics.Raycast(walkpoint, -transform.up, 2f, whatIsGround))
+            walkpointset = true;
+    }
+
+    private void ChasePlayer()
+    {
+        agent.SetDestination(player.position);
+    }
+
+    private void AttackPlayer()
+    {
+        agent.SetDestination(transform.position);
+
+        transform.LookAt(player);
+    }
+}
+*/
